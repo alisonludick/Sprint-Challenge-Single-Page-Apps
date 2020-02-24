5271fbc41644dadc7e712854c465f888f22699e9
Alison Ludick <alison.ludick@gmail.com>
2020-02-24 07:05:49 +0200

    refactored code in each file

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,28 @@
-import React from "react";
+import React, {useEffect } from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+	const {
+		name,
+		gender,
+		image,
+		species,
+		status,
+		type
+	} = props.data;
+
+	useEffect(() => {
+		return () => { };
+	}, [])
+	return (
+		<div className="character">
+			<div className="content">
+				<div><b>Name:</b> {name}</div>
+				<div><b>Gender:</b> {gender}</div>
+				<div><b>Species:</b> {species}</div>
+				{type ? <div><b>Type:</b> {type}</div> : null}
+				{status !== "unknown" ? <div><b>Status:</b> {status}</div> : null}
+			</div>
+			<img src={image} alt={type ? type : name} />
+		</div>
+	);
 }
